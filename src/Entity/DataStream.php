18add0ae89b3a8a3334668fbe74298e0c072d712
Artur JEDLIŃSKI <artur.jedlinski@rebel.pl>
2025-09-22 13:00:37 +0200

    php7.2

---
+++ src/Entity/DataStream.php
@@ -0,0 +1,41 @@
+<?php
+namespace Rebel\BCApi2\Entity;
+
+use Rebel\BCApi2\Client;
+use GuzzleHttp\Psr7;
+use Rebel\BCApi2\Request;
+use Rebel\BCApi2\Exception;
+
+class DataStream
+{
+    private $url;
+
+    public function __construct(
+            string $url)
+    {
+        $this->url = $url;
+    }
+    
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+    
+    public function downloadWith(Client $client): string
+    {
+        $response = $client->get($this->url);
+        return $response->getBody()->getContents();
+    }
+    
+    public function uploadWith(Client $client, string $data, ?string $etag = null): void
+    {
+        $response = $client->call(
+            new Psr7\Request('PATCH', $this->url, array_filter([
+                Request::HEADER_ETAG => $etag,
+                'Content-Type' => 'application/octet-stream'
+            ]), $data));
+        if ($response->getStatusCode() !== Client::HTTP_NO_CONTENT) {
+            throw new Exception\InvalidResponseException($response);
+        }
+    }
+}
